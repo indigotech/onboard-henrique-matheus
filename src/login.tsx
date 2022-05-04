51c935091ad5d58bfd94ab289877c6501f18765e
DamaralHenrique <rique01101999@gmail.com>
2022-05-04 15:05:49 -0300

    🎨 Fix file indentation

--- src/login.tsx
+++ src/login.tsx
@@ -4,49 +4,6 @@ import {TouchableOpacity, Text, View, TextInput} from 'react-native';
 import styled from 'styled-components';
 
 const Background = styled.View`
-	padding: 20px;
-	padding-top: 100px;
-`;
-const FieldsCell = styled.View`
-	padding-top: 100px;
-`;
-const TextBox = styled.TextInput`
-	margin: 10px;
-	padding: 10px;
-	border: 1px;
-	border-radius: 10px;
-	border-color: ${(props) => props.status === undefined ? 'black' : 'red'};
-`;
-const MainText = styled.Text`
-	font-size: 30px;
-	text-align: center;
-	padding: 10px;
-	color: black;
-`;
-const SubText = styled.Text`
-	font-size: 15px;
-	color: grey;
-`;
-const LoginButton = styled.TouchableOpacity`
-	background-color: #6d50f2;
-	padding: 10px;
-	border-radius: 10px;
-	margin-top: 20px;
-`;
-const ButtonText = styled.Text`
-	font-size: 20px;
-	text-align: center;
-	color: white;
-`;
-const ErrorText = styled.Text`
-	font-size: 12px;
-	color: red;
-	align-self: center;
-`;
-
-type FieldErrors = 'structure' | 'empty' | 'length';
-
-const Background = styled.View`
   padding: 20px;
   padding-top: 100px;
 `;
@@ -58,6 +15,7 @@ const TextBox = styled.TextInput`
   padding: 10px;
   border: 1px;
   border-radius: 10px;
+  border-color: ${(props) => props.status === undefined ? 'black' : 'red'};
 `;
 const MainText = styled.Text`
   font-size: 30px;
@@ -80,52 +38,59 @@ const ButtonText = styled.Text`
   text-align: center;
   color: white;
 `;
+const ErrorText = styled.Text`
+  font-size: 12px;
+  color: red;
+  align-self: center;
+`;
+
+type FieldErrors = 'structure' | 'empty' | 'length';
 
 const Login = () => {
 
-	const [email, setEmail] = useState<string>('');
-	const [password, setPassword] = useState<string>('');
-	const [emailError, setEmailError] = useState<FieldErrors>();
-	const [passwordError, setPasswordError] = useState<FieldErrors>();
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [emailError, setEmailError] = useState<FieldErrors>();
+  const [passwordError, setPasswordError] = useState<FieldErrors>();
 
-	function containsAnyLetter(str: string) {
-		return /[a-zA-Z]/.test(str);
-	}
-	function containsAnyDigit(str: string) {
-		return /[1-9]/.test(str);
-	}
+  function containsAnyLetter(str: string) {
+    return /[a-zA-Z]/.test(str);
+  }
+  function containsAnyDigit(str: string) {
+    return /[1-9]/.test(str);
+  }
 
-	const validateElmail = () => {
-		if (email.length === 0){
-			setEmailError('empty');
-		} else if (email.search('@') === -1){
-			setEmailError('structure');
-		} else {
-			var emailSplited = email.split('@',2);
-			if (!emailSplited[1].endsWith('.com') || emailSplited[1].length <= 4 || emailSplited[0].length === 0 ){
-				setEmailError('structure');
-			} else {
-				setEmailError(undefined);
-			}
-		}
-	};
+  const validateElmail = () => {
+    if (email.length === 0){
+      setEmailError('empty');
+    } else if (email.search('@') === -1){
+      setEmailError('structure');
+    } else {
+      var emailSplited = email.split('@',2);
+      if (!emailSplited[1].endsWith('.com') || emailSplited[1].length <= 4 || emailSplited[0].length === 0 ){
+        setEmailError('structure');
+      } else {
+        setEmailError(undefined);
+      }
+    }
+  };
 
-	const validatePassword = () => {
-		if (password.length === 0){
-			setPasswordError('empty');
-		} else if (password.length < 7){
-			setPasswordError('length');
-		} else if (containsAnyLetter(password) && containsAnyDigit(password)){
-			setPasswordError(undefined);
-		} else {
-			setPasswordError('structure');
-		}
-	};
+  const validatePassword = () => {
+    if (password.length === 0){
+      setPasswordError('empty');
+    } else if (password.length < 7){
+      setPasswordError('length');
+    } else if (containsAnyLetter(password) && containsAnyDigit(password)){
+      setPasswordError(undefined);
+    } else {
+      setPasswordError('structure');
+    } 
+  };
 
-	const validateLogin = () => {
-		validateElmail();
-		validatePassword();
-	};
+  const validateLogin = () => {
+    validateElmail();
+    validatePassword();
+  };
 
 	// this.render () {
 	return (
