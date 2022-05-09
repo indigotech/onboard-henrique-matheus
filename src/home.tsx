65b35f94a7779283bcb3d500948372494c55058c
DamaralHenrique <rique01101999@gmail.com>
2022-05-09 11:53:54 -0300

    ✨ Add get for user token

--- src/home.tsx
+++ src/home.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Navigation } from 'react-native-navigation';
+import { getUserToken } from './components/cache';
 
 const Home = () => {
-    return (
-      <View style={styles.root}>
-        <Text>Hello React Native Navigation</Text>
-      </View>
-    );
-  };
+
+  const [token, setToken] = useState('');
+  useEffect(() => {
+    const getInfo = async () => {
+      setToken(await getUserToken());
+    } 
+    getInfo();
+  });
+
+  return (
+    <View style={styles.root}>
+      <Text>Hello React Native Navigation</Text>
+      <Text>Yourt token: {token}</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   root: {
