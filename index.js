aec97149288b6dc29eafe739264c3751d8295fbd
DamaralHenrique <rique01101999@gmail.com>
2022-05-10 17:48:40 -0300

    🔥 Remove commets

--- index.js
+++ index.js
@@ -10,42 +10,15 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: 'https://tq-template-server-sample.herokuapp.com/graphql',
-  // uri: 'https://48p1r2roz4.sse.codesandbox.io',
   cache: new InMemoryCache()
 });
-  
-// class RenderComponent extends React.Component {
-//   render() {
-//     return (
-//       <View>
-//         {this.props.render()}
-//       </View>
-//     );
-//   }
-// }
 
-// function withApollo(Component) {
-//   return class extends React.Component {
-//     render() {
-//       return (
-//         <ApolloProvider client={client}>
-//           <RenderComponent render={() => (
-//             <Component {...this.props} client={client} />
-//           )}/>
-//         </ApolloProvider>
-//       );
-//     }
-//   }
-// }
-
-// Navigation.registerComponent('Login', () => withApollo(LoginPage));
 Navigation.registerComponent('Login', () => (props) =>
   <ApolloProvider client={client}>
     <LoginPage {...props} />
   </ApolloProvider>,
   () => LoginPage
 );
-// Navigation.registerComponent('Home', () => withApollo(HomePage));
 Navigation.registerComponent('Home', () => (props) =>
   <ApolloProvider client={client}>
     <HomePage {...props} />
