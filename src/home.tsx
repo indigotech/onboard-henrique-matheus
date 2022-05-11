b5173064ec0634ec0f82ab77d0f1e33d6a97a7d7
DamaralHenrique <rique01101999@gmail.com>
2022-05-11 10:53:13 -0300

    ✨ Add infinit list with load button

--- src/home.tsx
+++ src/home.tsx
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, FlatList, SafeAreaView } from 'react-native';
+import { View, Text, FlatList, SafeAreaView, Button } from 'react-native';
 import { ClientCard } from './components/client-card';
 import { getUserToken } from './utils/cache';
 import { MainContainer } from './utils/style';
 import { useUserList } from './utils/user-service';
 import styled from 'styled-components/native';
 
+const ClientsPerLoad = 20;
+
 export const HomePage = (props) => {
 
   const [token, setToken] = useState<any>();
-  const { loading, error, clientList, getClientList } = useUserList(token);
+  const [offset, setOffset] = useState(0);
+  const { loading, error, clientList, getClientList } = useUserList(token, offset, ClientsPerLoad);
+  const [ clientListDisplayed, setClientList] = useState<any[]>([]);
+
 
   useEffect(() => {
     const loadUserToken = async () => {
@@ -20,60 +25,31 @@ export const HomePage = (props) => {
     loadUserToken();
   },[]);
 
-  const clientsList = [
-    {
-      "name": "Yango Reis",
-      "email": "Yango16@hotmail.com"
-    },
-    {
-      "name": "Ladislau Pereira",
-      "email": "Dalila1@live.com"
-    },
-    {
-      "name": "Sr. Breno Oliveira",
-      "email": "Slvia_Carvalho@gmail.com"
-    },
-    {
-      "name": "Mércia Carvalho",
-      "email": "Sulen.Batista60@yahoo.com"
-    },
-    {
-      "name": "Mércia Braga Neto",
-      "email": "Pedro_Braga@gmail.com"
-    },
-    {
-      "name": "Dr. Bruna Moraes",
-      "email": "Ricardo.Pereira@yahoo.com"
-    },
-    {
-      "name": "Víctor Moraes",
-      "email": "Pablo96@live.com"
-    },
-    {
-      "name": "César Moreira",
-      "email": "Alexandre_Albuquerque65@hotmail.com"
-    },
-    {
-      "name": "Ígor Saraiva",
-      "email": "Joo96@live.com"
-    },
-    {
-      "name": "Sirineu Costa",
-      "email": "Marcos.Moreira@hotmail.com"
+  useEffect(() => {
+    if(clientListDisplayed){
+      setClientList(clientListDisplayed.concat(clientList));
+    }else{
+      setClientList(clientList);
     }
-  ];
+  },[clientList]);
 
   return (
     <SafeAreaView>
       <MainContainer>
       <Text>Lista de clientes</Text>
       {!loading &&
-        <FlatList
-          data={clientList}
-          renderItem={({ item }) => (
-            <ClientCard client={item}/>
-          )}
-        />
+        <View style={{height: '95%'}}>
+          <FlatList
+            data={clientListDisplayed}
+            renderItem={({ item }) => (
+              item && <ClientCard client={item}/>
+            )}
+            ListFooterComponent={() => (
+              <Button title="Carregar mais" onPress={() => setOffset(offset + ClientsPerLoad)}/>
+            )}
+            extraData={true}
+          />
+        </View>
       }
       {!loading && <Text>{error}</Text>}
       {loading && <Text>Loading...</Text>}
