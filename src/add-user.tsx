87de2af60747274115ae30c66717765cc2fa15bd
DamaralHenrique <rique01101999@gmail.com>
2022-05-12 10:40:06 -0300

    ✨ Add user form

--- src/add-user.tsx
+++ src/add-user.tsx
@@ -1,34 +1,29 @@
 import React, {useState, useEffect} from 'react';
 import {View} from 'react-native';
-import {
-  Background,
-  MainText,
-  FieldsCell,
-  SubText,
-  ButtonText,
-  ErrorText,
-  LoginButton,
-  TextBox,
-} from './utils/style';
+import { Background, MainText, FieldsCell, SubText, ButtonText, ErrorText, LoginButton, TextBox } from './utils/style';
 import {validateEmail, validatePassword} from './utils/string-validation';
 import {useLogin} from './utils/login-service';
 import {LoadingLayer} from './components/loading-layer';
 import {FieldErrors} from './utils/errors';
-import { EmailField, NameField, PhoneField } from './components/form-fields';
+import { EmailField, NameField, PhoneField, DateField, UserRoleField } from './components/form-fields';
 
 export const AddUserPage = props => {
   const [name, setName] = useState<string>('');
   const [email, setEmail] = useState<string>('');
   const [phone, setPhone] = useState<string>('');
+  const [birthDate, setBirthDate] = useState<string>('');
+  const [role, setRole] = useState<string>('');
 
   const [nameError, setNameError] = useState<FieldErrors>();
   const [emailError, setEmailError] = useState<FieldErrors>();
   const [phoneError, setPhoneError] = useState<FieldErrors>();
+  const [birthDateError, setBirthDateError] = useState<FieldErrors>();
+  const [roleError, setRoleError] = useState<FieldErrors>();
   const [loginError, setLoginError] = useState<string>('');
 
   const {login, loading} = useLogin();
 
-  const validateLogin = () => {
+  const addUser = () => {
     setLoginError('');
     const validEmail = validateEmail(email, setEmailError);
     // const validPassword = validatePassword(password, setPasswordError);
@@ -42,10 +37,12 @@ export const AddUserPage = props => {
       <Background>
         <FieldsCell>
           <NameField setvalue={setName} value={name} error={nameError} />
+          <PhoneField setvalue={setPhone} value={phone} error={phoneError} setError={setPhoneError}/>
+          <DateField setValue={setBirthDate} value={birthDate} error={birthDateError} />
           <EmailField setvalue={setEmail} value={email} error={emailError} />
-          <PhoneField setvalue={setPhone} value={phone} error={phoneError} />
+          <UserRoleField setValue={setRole} value={role} error={roleError}/>
           <LoginButton>
-            <ButtonText onPress={() => validateLogin()}>Cadastrar</ButtonText>
+            <ButtonText onPress={() => addUser()}>Cadastrar</ButtonText>
           </LoginButton>
           <ErrorText>{loginError}</ErrorText>
         </FieldsCell>
--- src/components/form-fields.tsx
+++ src/components/form-fields.tsx
@@ -4,10 +4,11 @@ import {FieldErrors} from '../utils/errors';
 import {SubText, TextBox, ErrorText} from '../utils/style';
 import PhoneInput, {
   isValidNumber,
-  getCallingCode,
   getCountryCode,
 } from 'react-native-phone-number-input';
 import styled from 'styled-components/native';
+import DatePicker from 'react-native-datepicker';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 export const EmailField = props => {
   return (
@@ -49,7 +50,6 @@ export const NameField = props => {
 
 export const PhoneField = props => {
   const phoneInput = useRef<PhoneInput>(null);
-  const [valid, setValid] = useState(false);
 
   return (
     <View>
@@ -57,11 +57,99 @@ export const PhoneField = props => {
       <PhoneInput
         onChangeFormattedText={value => {
           props.setvalue(value);
-          setValid(
-            isValidNumber(value, phoneInput.current?.getCountryCode(value)),
-          );
+          isValidNumber(value, phoneInput.current?.getCountryCode(value))
+            ? props.setError(undefined)
+            : props.setError(FieldErrors.structure);
         }}
         ref={phoneInput}
+      />
+      <ErrorText>
+        {props.error === FieldErrors.empty && 'Este campo é obrigatório'}
+        {props.error === FieldErrors.structure && 'Número inválido'}
+        {!props.error && ' '}
+      </ErrorText>
+    </View>
+  );
+};
+
+export const DateField = props => {
+  let today = new Date();
+  let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+  let minDate = today.getFullYear() - 120 + '-01-01';
+
+  return (
+    <View>
+      <SubText>Data de nascimento</SubText>
+      <DatePicker
+        style={{width: '100%'}}
+        date={props.value}
+        mode="date"
+        placeholder="Seleciona uma data"
+        format="YYYY-MM-DD"
+        minDate={minDate}
+        maxDate={date}
+        confirmBtnText="Confirmar"
+        cancelBtnText="Cancelar"
+        useNativeDriver={true}
+        customStyles={{
+          dateIcon: {
+            position: 'absolute',
+            left: 15,
+            top: 4,
+            marginLeft: 0,
+          },
+          dateInput: {
+            marginLeft: 51,
+            margin: 10,
+            padding: 10,
+            border: 1,
+            borderRadius: 10,
+            borderColor: props.error === undefined ? 'black' : 'red',
+          },
+        }}
+        onDateChange={value => {
+          props.setValue(value);
+        }}
+      />
+      <ErrorText>
+        {props.error === FieldErrors.empty && 'Este campo é obrigatório'}
+        {!props.error && ' '}
+      </ErrorText>
+    </View>
+  );
+};
+
+export const UserRoleField = props => {
+  const [open, setOpen] = useState(false);
+  const [items, setItems] = useState([
+    {label: 'admin', value: 'admin'},
+    {label: 'user', value: 'user'}
+  ]);
+
+  return (
+    <View>
+      <SubText>Cargo</SubText>
+      <DropDownPicker
+        open={open}
+        value={props.value}
+        items={items}
+        setOpen={setOpen}
+        setValue={props.setValue}
+        setItems={setItems}
+        onChangeValue={(value) => {
+          console.log(value);
+        }}
+        dropDownDirection="TOP"
+        dropDownContainerStyle={{
+          backgroundColor: "white",
+          alignSelf: 'center',
+        }}
+        containerStyle={{
+          padding: 10,
+        }}
+        style={{
+          borderRadius: 10
+        }}
       />
       <ErrorText>
         {props.error === FieldErrors.empty && 'Este campo é obrigatório'}
