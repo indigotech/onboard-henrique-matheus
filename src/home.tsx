e7df0de71dfee9dbed7dda755260b938abbc122c
DamaralHenrique <rique01101999@gmail.com>
2022-05-12 16:04:22 -0300

    🎨 Improve code format

--- src/home.tsx
+++ src/home.tsx
@@ -13,7 +13,7 @@ export const HomePage = (props) => {
   const [token, setToken] = useState<any>();
   const [offset, setOffset] = useState(0);
   const { loading, error, clientList, getClientList } = useUserList(token, offset, ClientsPerLoad);
-  const [ clientListDisplayed, setClientList] = useState<any[]>([]);
+  const [ clientListDisplayed, setClientListDisplayed] = useState<any[]>([]);
 
 
   useEffect(() => {
@@ -26,7 +26,9 @@ export const HomePage = (props) => {
   },[]);
 
   useEffect(() => {
-    if (clientList) setClientList(clientListDisplayed.concat(clientList));
+    if (clientList) {
+      setClientListDisplayed((currentList) => currentList ? currentList.concat(clientList) : clientList)
+    }
   },[clientList]);
 
   return (
