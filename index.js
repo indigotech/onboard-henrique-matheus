8f551fd37a00c7c48b99c080fd65c84c7848f0af
DamaralHenrique <rique01101999@gmail.com>
2022-05-13 14:44:03 -0300

    ⚡️ Improve Apolloprovider setup

--- index.js
+++ index.js
@@ -8,29 +8,40 @@ import { DetailsPage } from './src/details';
 import React from 'react';
 import { Navigation } from 'react-native-navigation';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { View } from 'react-native';
 
 const client = new ApolloClient({
   uri: 'https://tq-template-server-sample.herokuapp.com/graphql',
   cache: new InMemoryCache()
 });
-Navigation.registerComponent('Login', () => (props) =>
-  <ApolloProvider client={client}>
-    <LoginPage {...props} />
-  </ApolloProvider>,
-  () => LoginPage
-);
-Navigation.registerComponent('Home', () => (props) =>
-  <ApolloProvider client={client}>
-    <HomePage {...props} />
-  </ApolloProvider>,
-  () => HomePage
-);
-Navigation.registerComponent('UserDetails', () => (props) =>
-  <ApolloProvider client={client}>
-    <DetailsPage {...props} />
-  </ApolloProvider>,
-  () => DetailsPage
-);
+
+class RenderComponent extends React.Component {
+  render() {
+    return (
+      <View>
+        {this.props.render()}
+      </View>
+    );
+  }
+}
+
+function addApollo(Component) {
+  return class extends React.Component {
+    render() {
+      return (
+        <ApolloProvider client={client}>
+          <RenderComponent render={() => (
+            <Component {...this.props} client={client} />
+          )}/>
+        </ApolloProvider>
+      );
+    }
+  }
+}
+
+Navigation.registerComponent('Login', () => addApollo(LoginPage));
+Navigation.registerComponent('Home', () => addApollo(HomePage));
+Navigation.registerComponent('UserDetails', () => addApollo(DetailsPage));
 
 Navigation.events().registerAppLaunchedListener(async () => {
   Navigation.setRoot({
